5b28cec9d0a62e9af06c4f9f4fe48b8bcfa3f532
tanbirahmadfr <018tanbirahmad@gmail.com>
2022-08-12 17:41:02 +0600

    first commit

---
+++ js/script.js
@@ -0,0 +1,85 @@
+// ====banner part start ====
+$('.venobox').venobox()
+
+// ====banner part end ====
+
+// ======review part start ======
+
+$('.cyclist_slider').slick({
+    dots: false,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay:true,
+    arrows:false,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          infinite: true,
+          dots: false
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+    ]
+  });
+// ======review part end ======
+
+
+ // ========back to top part ======
+ var back_to_top = document.querySelector(".back_to_top")
+
+ window.addEventListener("scroll",function(){
+ 
+   back_to_top.classList.toggle("bottom_to_top",window.scrollY > 150)
+ })
+ // ========back to top part ======
+
+ 
+ // =========sticky menu part ========
+var main_menu = document.getElementById("main_menu");
+window.addEventListener("scroll",function(){
+main_menu.classList.toggle("sticky",window.scrollY > 100);
+})
+// =========sticky menu part ========
+
+
+// ========scroll active part start =======
+
+
+const sections = document.querySelectorAll('section[id]')
+
+function scrollActive(){
+    const scrollY = window.pageYOffset
+
+    sections.forEach(current =>{
+        const sectionHeight = current.offsetHeight
+        const sectionTop = current.offsetTop - 50;
+        sectionId = current.getAttribute('id')
+
+        if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
+            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.add('active-link')
+        }else{
+            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.remove('active-link')
+        }
+    })
+}
+window.addEventListener('scroll', scrollActive)
+
+// =======scroll active part end =======
